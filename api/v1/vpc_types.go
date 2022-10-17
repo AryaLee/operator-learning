77f433d621be9cbd4556077b0dab987801aee016
meiyunli <meiyun.li@smartx.com>
2022-10-17 14:42:58 +0800

    create vpc api

---
+++ api/v1/vpc_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022 ArayLee.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// VpcSpec defines the desired state of Vpc
+type VpcSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Vpc. Edit vpc_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// VpcStatus defines the observed state of Vpc
+type VpcStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Vpc is the Schema for the vpcs API
+type Vpc struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VpcSpec   `json:"spec,omitempty"`
+	Status VpcStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// VpcList contains a list of Vpc
+type VpcList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Vpc `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Vpc{}, &VpcList{})
+}
